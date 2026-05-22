e477f44ed4c31abdd94c38a028be272bf801f71a
Ofek Kanari <ofekgki@gmail.com>
2026-05-22 17:44:54 +0300

    Initial Commit

---
+++ Submission1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Submission1
+//
+//  Created by Ofek Kanari on 22/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
