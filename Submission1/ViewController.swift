fe828a5efec4860c2f7532d255912115373c810f
Ofek Kanari <ofekgki@gmail.com>
2026-05-22 18:49:36 +0300

    Initail Commit

--- Submission1/ViewController.swift
+++ Submission1/ViewController.swift
@@ -9,6 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var main_LBL_east: UILabel!
+    
+    @IBOutlet weak var main_IMG_east: UIImageView!
+    
+    @IBOutlet weak var main_LBL_west: UILabel!
+    
+    @IBOutlet weak var main_IMG_west: UIImageView!
+    
+    @IBOutlet weak var main_LBL_enterName: UILabel!
+    
+    @IBOutlet weak var main_LBL_name: UILabel!
+    
+    @IBOutlet weak var main_BTN_start: UIButton!
+    
+    var playerName : String = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
