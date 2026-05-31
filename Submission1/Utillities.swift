9269db421b227afc9b27244fa0b541f7fdcfd69c
Ofek Kanari <ofekgki@gmail.com>
2026-05-31 13:40:57 +0300

    Added Scenes, Started work on start logic

---
+++ Submission1/Utillities.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+class ClockUtil{
+    
+    private var count :Int = 0
+    
+    var callBackClock: CallBackClock?
+    
+    var timer: Timer?
+    
+    func start(){
+        
+        timer = Timer.scheduledTimer(
+        timeInterval: 1.0,
+         target: self,
+         selector: #selector(tickAccourd),
+         userInfo: nil,
+         repeats: true)
+        
+    }
+        
+    
+    
+    func stop(){
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    @objc private func tickAccourd(){
+        
+        if count < 5{
+            count += 1
+            callBackClock?.tick(ticks: count)
+        }
+        
+        else {
+            count = 0
+            callBackClock?.tick(ticks: count)
+
+        }
+    
+    }
+    
+}
+
+protocol CallBackClock{
+    func tick(ticks: Int)
+    
+}
